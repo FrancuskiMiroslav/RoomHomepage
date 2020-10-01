0bc9bdbf9d1793bc0305e6502c1fa78ed9b0d8a3
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-10-02 00:57:36 +0200

    added sticky responsive navbar with hamburger

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+	const menuBtn = document.getElementById('menu-btn');
+	const nav = document.getElementById('nav');
+	const navContainer = document.getElementById('nav-container');
+	const logo = document.getElementById('logo');
+
+	menuBtn.addEventListener('click', (e) => {
+		menuBtn.classList.toggle('clicked');
+		nav.classList.toggle('show');
+		navContainer.classList.toggle('overlay');
+		logo.classList.toggle('hide');
+	});
+
+	window.addEventListener('scroll', (e) => {
+		if (document.documentElement.scrollTop > 0) {
+			navContainer.classList.add('sticky');
+		} else {
+			navContainer.classList.remove('sticky');
+		}
+	});
+});
